abcef5b784dc1515ed4def98224aadedae8d2f0f
Thomas ROBERT <thomas_3004@hotmail.fr>
2026-05-25 08:14:14 +0200

    chore: setup oxlint

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import App from './App.tsx';
 
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
